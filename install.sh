3d56444e0b2bea6ab4983002cf73440bac61885a
Yash Eksambekar <yasheksambekar@gmail.com>
2022-07-11 11:01:21 +0530

    modified install script

--- install.sh
+++ install.sh
@@ -14,86 +14,86 @@ EOF
 
 # Install apt packages
 sudo apt update
-sudo apt -qq install gpg
-sudo apt -qq install thunar
-sudo apt -qq install rofi
-sudo apt -qq install feh
-sudo apt -qq install i3blocks
-sudo apt -qq install curl
-sudo apt -qq install wget
-sudo apt -qq install apt-transport-https
-sudo apt -qq install dirmngr
-sudo apt -qq install openssh-server
-sudo apt -qq install gcc
-sudo apt -qq install g++
-sudo apt -qq install default-jdk
-sudo apt -qq install i3
-sudo apt -qq install xorg
-sudo apt -qq install vim
-sudo apt -qq install tmux
-sudo apt -qq install lightdm
-sudo apt -qq install slick-greeter
-sudo apt -qq install i3lock
-sudo apt -qq install compton
-sudo apt -qq install scrot
-sudo apt -qq install imagemagick
-sudo apt -qq install snapd
-sudo apt -qq install neofetch
-sudo apt -qq install lxappearance
-sudo apt -qq install ffmpeg
-sudo apt -qq install obs-studio
-sudo apt -qq install pulseaudio
-sudo apt -qq install pavucontrol
-sudo apt -qq install python3-pip
-sudo apt -qq install gnome-screenshot
-sudo apt -qq install firmware-linux
-sudo apt -qq install firmware-linux-nonfree
-sudo apt -qq install evince
-sudo apt -qq install yarn
-sudo apt -qq install transmission-gtk
-sudo apt -qq install git
-sudo apt -qq install handbrake
-sudo apt -qq install nasm
-sudo apt -qq install adb
-sudo apt -qq install firefox-esr
-sudo apt -qq install vlc
-sudo apt -qq install qemu-kvm
-sudo apt -qq install libvirt-clients
-sudo apt -qq install libvirt-daemon-system
-sudo apt -qq install bridge-utils
-sudo apt -qq install virtinst
-sudo apt -qq install libvirt-daemon
-sudo apt -qq install virt-manager
-sudo apt -qq install rxvt-unicode
-sudo apt -qq install libdrm-dev
-sudo apt -qq install cmake
-sudo apt -qq install libqt5svg5
-sudo apt -qq install qml-module-qtquick-controls
-sudo apt -qq install qml-module-qtquick-controls2
-sudo apt -qq install qml-module-qtquick-controls
-sudo apt -qq install qml-module-qtquick-layouts
-sudo apt -qq install qml-module-qtcharts
-sudo apt -qq install qml-module-qt-labs-platform
-sudo apt -qq install libkf5archive5
-sudo apt -qq install extra-cmake-modules
-sudo apt -qq install libqt5quickcontrols2-5
-sudo apt -qq install qtdeclarative5-dev
-sudo apt -qq install libqt5charts5-dev
-sudo apt -qq install libqt5svg5-dev
-sudo apt -qq install qttools5-dev
-sudo apt -qq install qttools5-dev-tools
-sudo apt -qq install libkf5auth-dev
-sudo apt -qq install libkf5archive-dev
-sudo apt -qq install libbotan-2-dev
-sudo apt -qq install libbotan-2-doc
-sudo apt -qq install python3-botan
-sudo apt -qq install qtdeclarative5-dev
-sudo apt -qq install libqt5charts5-dev
-sudo apt -qq install libqt5svg5-dev
-sudo apt -qq install sysvinit-utils
-sudo apt -qq install hwdata
-sudo apt -qq install mesa-utils
-sudo apt -qq install util-linux
+sudo apt -qqy install gpg
+sudo apt -qqy install thunar
+sudo apt -qqy install rofi
+sudo apt -qqy install feh
+sudo apt -qqy install i3blocks
+sudo apt -qqy install curl
+sudo apt -qqy install wget
+sudo apt -qqy install apt-transport-https
+sudo apt -qqy install dirmngr
+sudo apt -qqy install openssh-server
+sudo apt -qqy install gcc
+sudo apt -qqy install g++
+sudo apt -qqy install default-jdk
+sudo apt -qqy install i3
+sudo apt -qqy install xorg
+sudo apt -qqy install vim
+sudo apt -qqy install tmux
+sudo apt -qqy install lightdm
+sudo apt -qqy install slick-greeter
+sudo apt -qqy install i3lock
+sudo apt -qqy install compton
+sudo apt -qqy install scrot
+sudo apt -qqy install imagemagick
+sudo apt -qqy install snapd
+sudo apt -qqy install neofetch
+sudo apt -qqy install lxappearance
+sudo apt -qqy install ffmpeg
+sudo apt -qqy install obs-studio
+sudo apt -qqy install pulseaudio
+sudo apt -qqy install pavucontrol
+sudo apt -qqy install python3-pip
+sudo apt -qqy install gnome-screenshot
+sudo apt -qqy install firmware-linux
+sudo apt -qqy install firmware-linux-nonfree
+sudo apt -qqy install evince
+sudo apt -qqy install yarn
+sudo apt -qqy install transmission-gtk
+sudo apt -qqy install git
+sudo apt -qqy install handbrake
+sudo apt -qqy install nasm
+sudo apt -qqy install adb
+sudo apt -qqy install firefox-esr
+sudo apt -qqy install vlc
+sudo apt -qqy install qemu-kvm
+sudo apt -qqy install libvirt-clients
+sudo apt -qqy install libvirt-daemon-system
+sudo apt -qqy install bridge-utils
+sudo apt -qqy install virtinst
+sudo apt -qqy install libvirt-daemon
+sudo apt -qqy install virt-manager
+sudo apt -qqy install rxvt-unicode
+sudo apt -qqy install libdrm-dev
+sudo apt -qqy install cmake
+sudo apt -qqy install libqt5svg5
+sudo apt -qqy install qml-module-qtquick-controls
+sudo apt -qqy install qml-module-qtquick-controls2
+sudo apt -qqy install qml-module-qtquick-controls
+sudo apt -qqy install qml-module-qtquick-layouts
+sudo apt -qqy install qml-module-qtcharts
+sudo apt -qqy install qml-module-qt-labs-platform
+sudo apt -qqy install libkf5archive5
+sudo apt -qqy install extra-cmake-modules
+sudo apt -qqy install libqt5quickcontrols2-5
+sudo apt -qqy install qtdeclarative5-dev
+sudo apt -qqy install libqt5charts5-dev
+sudo apt -qqy install libqt5svg5-dev
+sudo apt -qqy install qttools5-dev
+sudo apt -qqy install qttools5-dev-tools
+sudo apt -qqy install libkf5auth-dev
+sudo apt -qqy install libkf5archive-dev
+sudo apt -qqy install libbotan-2-dev
+sudo apt -qqy install libbotan-2-doc
+sudo apt -qqy install python3-botan
+sudo apt -qqy install qtdeclarative5-dev
+sudo apt -qqy install libqt5charts5-dev
+sudo apt -qqy install libqt5svg5-dev
+sudo apt -qqy install sysvinit-utils
+sudo apt -qqy install hwdata
+sudo apt -qqy install mesa-utils
+sudo apt -qqy install util-linux
 
 # Install snap packages
 sudo snap install core
@@ -126,14 +126,14 @@ sudo apt install code
 
 # Install Chrome
 wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
-sudo apt -qq install ./chrome.deb
+sudo apt -qqy install ./chrome.deb
 sudo rm -f ./chrome.deb
 
 # Install Sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
-sudo apt -qq install sublime-text
+sudo apt -qqy install sublime-text
 
 # Configure slick-greeter
 sudo tee /etc/lightdm/slick-greeter.conf<<EOF
